a083386773d49412494a890d204b8868d4f5f996
Aniket Harshe <aniket.umesh.harshe@gds.ey.com>
2023-12-14 18:39:09 +0530

    Added prop types

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image } from "react-bootstrap";
 import CardRow from "./CardRow";
+import PropTypes from "prop-types";
 
 const Card = ({ image, row1, row2, row3 }) => {
   return (
@@ -13,6 +14,13 @@ const Card = ({ image, row1, row2, row3 }) => {
       <CardRow row={row3} />
     </div>
   );
+};
+
+Card.propTypes = {
+  image: PropTypes.object,
+  row1: PropTypes.object,
+  row2: PropTypes.object,
+  row3: PropTypes.object,
 };
 
 export default Card;
